5a096d6379a2f5c293854294e600cd861812ecc2
Дарья Иванова <dashann@mail.ru>
2022-06-23 00:23:59 +0300

    feat/add Card component

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Cards.css';
+import {cardList} from '../../utils/constants/cardList';
+import Card from '../Card/Card';
 
 export default function Cards() {
   return (
     <section className="cards">
-      
+      <ul className="cards__list">
+        {cardList.map(card => (
+          <Card card={card} key={card.id}/>
+        ))}
+      </ul>
+
     </section>
   );
 }
